41e00af8d482fa0747cb24db2b03964ae87b34b3
ethanocurtis <ethancurtis10@yahoo.com>
2024-03-29 00:47:12 -0500

    bug Fixes

--- mcstatus/mcstatus_cog.py
+++ mcstatus/mcstatus_cog.py
@@ -1,8 +1,3 @@
-import discord
-from discord.ext import commands, tasks
-import mcstatus
-import asyncio
-
 class MinecraftCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -17,8 +12,11 @@ class MinecraftCog(commands.Cog):
         # Stop the task when the cog is unloaded
         self.update_server_status.cancel()
 
-    @tasks.loop(seconds=self.server_query_interval)
+    @tasks.loop(seconds=300)  # Default interval, will be changed in before_update_server_status
     async def update_server_status(self):
+        await self.update_status()
+
+    async def update_status(self):
         for server_address, channel_id in zip(self.server_addresses, self.server_status_channel_ids):
             try:
                 server = mcstatus.MinecraftServer.lookup(server_address)
@@ -50,54 +48,6 @@ class MinecraftCog(commands.Cog):
     @update_server_status.before_loop
     async def before_update_server_status(self):
         await self.bot.wait_until_ready()
+        self.update_server_status.change_interval(seconds=self.server_query_interval)
 
-    @commands.command()
-    async def server_status(self, ctx):
-        for server_address, channel_id in zip(self.server_addresses, self.server_status_channel_ids):
-            try:
-                server = mcstatus.MinecraftServer.lookup(server_address)
-                status = server.status()
-
-                # Format the server status information into an embed
-                embed = discord.Embed(title="Minecraft Server Status", color=discord.Color.green())
-                embed.add_field(name="Server", value=server_address)
-                embed.add_field(name="Players Online", value=status.players.online)
-                if status.players.sample:
-                    player_names = "\n".join(player.name for player in status.players.sample)
-                    embed.add_field(name="Player Names", value=player_names, inline=False)
-                embed.add_field(name="Max Players", value=status.players.max)
-                embed.add_field(name="Version", value=status.version.name)
-
-                await ctx.send(embed=embed)
-            except Exception as e:
-                await ctx.send(f"Error getting server status: {e}")
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def set_server_info(self, ctx, channel: discord.TextChannel, server_ip: str):
-        self.server_status_channel_ids.append(channel.id)
-        self.server_addresses.append(server_ip)
-        await ctx.send("Server information added.")
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def remove_server_info(self, ctx, index: int):
-        if index < len(self.server_addresses):
-            del self.server_status_channel_ids[index]
-            del self.server_addresses[index]
-            await ctx.send("Server information removed.")
-        else:
-            await ctx.send("Invalid server index.")
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def remove_channel_info(self, ctx, index: int):
-        if index < len(self.server_status_channel_ids):
-            del self.server_status_channel_ids[index]
-            del self.server_addresses[index]
-            await ctx.send("Channel information removed.")
-        else:
-            await ctx.send("Invalid channel index.")
-
-def setup(bot):
-    bot.add_cog(MinecraftCog(bot))
+    # The rest of your commands remain the same
